8284fc090e0401ddfb41aa696294a75dfd2f6e8b
Rich <richbport@yahoo.com>
2025-12-02 12:00:56 -0500

    form reset priority default

--- script2.js
+++ script2.js
@@ -27,3 +27,7 @@ const month = String(now.getMonth() + 1).padStart(2, "0");
 const day = String(now.getDate()).padStart(2, "0");
 const year = now.getFullYear();
 taskDateInput.value = `${month}/${day}/${year}`;
+
+document.querySelectorAll('input[name="priority"]').forEach((radio) => {
+    radio.checked = radio.value === "low";
+})
